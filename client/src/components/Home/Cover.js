81c2a41fce2672d8ff433c94a0d181578de19c4a
CarliC <carlic.dev@gmail.com>
2020-09-02 08:14:01 -0500

    small changes

--- client/src/components/Home/Cover.js
+++ client/src/components/Home/Cover.js
@@ -8,16 +8,22 @@ const Cover = () => {
             <div className='text-center w-full mb-4'>
             <p className='text-gray-100 inline font-semibold text-6xl mb-4'>Una <span className='inline text-green-500'><BsFillDropletFill className='inline'/></span> de vida<span className='text-green-500 font-bold text-7xl'>.</span></p>
             </div>
+            <div className='container text-center'>
+                <div className='block'>
                 <Link to='/login'>
-                    <button className='w-48 bg-green-500 px-2 py-1 hover:bg-green-600 lg:h-10 text-white  mr-1 rounded shadow-md'>
+                    <button className='w-64 bg-green-500 px-2 py-1 hover:bg-green-600 lg:h-10 text-white rounded shadow-md font-semibold focus:outline-none'>
                         Login
                     </button>   
                 </Link>
+                </div>
+                <div className='block mt-2'>
                 <Link to='/store'>
-                    <button className='w-48 bg-green-500 px-2 py-1 hover:bg-green-600 lg:h-10 text-white  ml-1 rounded shadow-md'>
+                    <button className='w-64  bg-green-500 px-2 py-1 hover:bg-green-600 lg:h-10 text-white rounded shadow-md font-semibold focus:outline-none'>
                         Tienda
                     </button>
-                </Link>           
+                </Link>
+                </div>  
+                </div>   
             <div className='text-center text-white w-full mt-1'>
                 <Link to='/register'>
                 Registro
